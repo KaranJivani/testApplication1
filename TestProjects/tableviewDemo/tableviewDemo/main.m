acbe3200d7825e1cc73426fe953b4b2b1b820f8a
KaranJivani <karanjivani@gmail.com>
2016-06-20 11:16:40 -0700

    Added project

---
+++ TestProjects/tableviewDemo/tableviewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tableviewDemo
+//
+//  Created by Karan Jivani on 6/20/16.
+//  Copyright © 2016 Karan Jivani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
